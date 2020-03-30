004d95f06cebe7965cc95ff57b8ad91929203fbc
Colin <cdbusby@gmail.com>
2020-03-29 22:44:46 -0300

    Remove unused CSS.

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,18 @@ module.exports = {
         baseUrl: 'https://drupal.colinbusby.com/',
         apiBase: 'jsonapi', // endpoint of Drupal server
       },
+    },
+    // Add after these plugins if used
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
     }
   ],
 }
