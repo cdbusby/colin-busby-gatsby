db4d20b47a202216c1a2bf48baefdb7560658015
Colin Busby <cdbusby@gmail.com>
2021-04-20 19:35:30 +0000

    Add CF analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "7509013dc50845ce9c0bb9fc14fe95ea"}'></script>
+  ])
+}
